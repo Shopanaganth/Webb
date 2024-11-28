f29f0c301b9dd25d9df3484b2039ff2218d63fc0
Shopanaganth <shopu02001@gmail.com>
2024-11-28 20:23:38 +0530

    Add new

--- script.js
+++ script.js
@@ -1034,19 +1034,19 @@ requiredSubjects: ["Business Studies","Economics","Accounting","Agricultural Sci
     name: "BIOMEDICAL TECHNOLOGY", 
     university: "The Gampaha Wickramarachchi University of Indigenous ", 
     districtRequirements: {'Colombo': 1.0057, 'Gampaha': 1.0278, 'Kalutara': 1.1620, 'Matale': 1.1511, 'Kandy': 1.1061, 'Nuwara Eliya': 0.9012, 'Galle': 1.1831, 'Matara': 1.2344, 'Hambantota': 1.1551, 'Jaffna': 0.8457, 'Kilinochchi': 0.7141, 'Mannar': 0.7467, 'Mullaitivu': 0.8908, 'Vavuniya': 1.0332, 'Trincomalee': 1.1359, 'Batticaloa': 1.2084, 'Ampara': 0.9017, 'Puttalam': 0.7273, 'Kurunegala': 1.1566, 'Anuradhapura': 0.9687, 'Polonnaruwa': 1.1071, 'Badulla': 1.1073, 'Monaragala': 0.8528, 'Kegalle': 1.1261, 'Ratnapura': 1.0580},
-    requiredSubjects: ["Engineering Technology ", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accounting", "Mathematics", "Biosystems Technology "]
+    requiredSubjects: ["Engineering Technology ", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accounting", "Mathematics", "Bio Systems Technology "]
 },
 { 
     name: "INDIGENOUS PHARMACEUTICAL TECHNOLOGY", 
     university: "The Gampaha Wickramarachchi University of Indigenous  ", 
     districtRequirements: {'Colombo': 0.9292, 'Gampaha': 0.9821, 'Kalutara': 1.0414, 'Matale': 1.1091, 'Kandy': 1.0226, 'Nuwara Eliya': 0.8341, 'Galle': 1.0864, 'Matara': 1.1012, 'Hambantota': 1.0918, 'Jaffna': 0.8371, 'Kilinochchi': 0.6293, 'Mannar': 0.8008, 'Mullaitivu': 0.4234, 'Vavuniya': 0.8293, 'Trincomalee': 0.6262, 'Batticaloa': 0.9611, 'Ampara': 0.9874, 'Puttalam': 0.6753, 'Kurunegala': 1.0954, 'Anuradhapura': 0.9214, 'Polonnaruwa': 1.0940, 'Badulla': 1.0919, 'Monaragala': 0.8524, 'Kegalle': 1.1031, 'Ratnapura': 1.0389},
-    requiredSubjects: ["Engineering Technology ", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accounting", "Mathematics", "Biosystems Technology "]
+    requiredSubjects: ["Engineering Technology ", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accounting", "Mathematics", "Bio Systems Technology "]
 },
 { 
     name: "YOGA AND PARAPSYCHOLOGY", 
     university: "The Gampaha Wickramarachchi University of Indigenous  ", 
     districtRequirements: {'Colombo': 1.2371, 'Gampaha': 1.3088, 'Kalutara': 1.3075, 'Matale': 1.3078, 'Kandy': 1.3141, 'Nuwara Eliya': 1.3351, 'Galle': 1.3263, 'Matara': 1.3521, 'Hambantota': 1.3120, 'Jaffna': 1.1645, 'Kilinochchi': 1.1550, 'Mannar': 1.3570, 'Mullaitivu': 1.1545, 'Vavuniya': 1.3099, 'Trincomalee': 1.2732, 'Batticaloa': 1.1742, 'Ampara': 1.2965, 'Puttalam': 1.2870, 'Kurunegala': 1.3155, 'Anuradhapura': 1.2951, 'Polonnaruwa': 1.2805, 'Badulla': 1.3169, 'Monaragala': 1.3544, 'Kegalle': 1.3169, 'Ratnapura': 1.2958},
-    requiredSubjects: ["Engineering Technology", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accountancy", "Mathematics", "Biosystems Technology "]
+    requiredSubjects: ["Engineering Technology", "Science for Technology", "Economics", "Geography", "Home Economics", "English", "Communication and Media Studies", "Information & Communication Technology", "Art", "Business Studies", "Agricultural Science", "Accountancy", "Mathematics", "Bio Systems Technology "]
 },
 { 
     name: "SOCIAL STUDIES IN INDIGENOUS KNOWLEDGE ", 
